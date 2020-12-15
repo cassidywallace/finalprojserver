e5242a02bbfb576d66112af97936dbb74fd86176
cassidywallace <cwalla53@uncc.edu>
2020-12-15 15:25:15 -0500

    added real ip address

--- testing.js
+++ testing.js
@@ -1,13 +1,13 @@
 const axios = require('axios').default
 token = ""
-axios.get('http://localhost:3001/login').then((res) => {
+axios.get('http://104.236.14.183:3001/login').then((res) => {
     token = res.data.token
     const config = {
         headers: { Authorization: `Bearer ${token}`}
     };
 
     axios.get(
-        'http://localhost:3001/budget',
+        'http://104.236.14.183:3001/budget',
         config
         ).then((res) => {
             console.log(res.data)
